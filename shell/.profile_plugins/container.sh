6fff335d4adf59b7accd05f3f6f0785a00b63ec8
David Oberacker <oberacker@fzi.de>
2025-06-06 08:55:47 +0200

    Add container aliases.

---
+++ shell/.profile_plugins/container.sh
@@ -0,0 +1,14 @@
+# stuff for easier handling my containers
+
+CONTAINER_USER="ubuntu"
+
+function fzirobdev() {
+  devcontainer up --workspace-folder $HOME/repos/devcontainer/humble --dotfiles-repository=https://github.com/Oberacda/dotfiles.git --dotfiles-install-command=install.sh
+  devcontainer exec --workspace-folder $HOME/repos/devcontainer/humble zsh
+}
+
+alias devup='devcontainer up --workspace-folder . --dotfiles-repository=https://github.com/Oberacda/dotfiles.git --dotfiles-install-command=install.sh'
+alias devupnodot='devcontainer up --workspace-folder .'
+alias devnew='devcontainer up --workspace-folder . --dotfiles-repository=https://github.com/Oberacda/dotfiles.git --dotfiles-install-command=install.bash --remove-existing-container'
+alias devrun='devcontainer exec --workspace-folder .'
+alias devsh='devcontainer exec --workspace-folder . zsh'
