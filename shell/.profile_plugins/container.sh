dc88ced112bccb7b6dc59802cc7ad3531791a318
David Oberacker <oberacker@fzi.de>
2025-06-24 10:08:09 +0200

    Update shell config.

--- shell/.profile_plugins/container.sh
+++ shell/.profile_plugins/container.sh
@@ -7,6 +7,7 @@ function fzirobdev() {
   devcontainer exec --workspace-folder $HOME/repos/devcontainer/humble zsh
 }
 
+alias cddev='cd $HOME/repos/devcontainer'
 alias devup='devcontainer up --workspace-folder . --dotfiles-repository=https://github.com/Oberacda/dotfiles.git --dotfiles-install-command=install.sh'
 alias devupnodot='devcontainer up --workspace-folder .'
 alias devnew='devcontainer up --workspace-folder . --dotfiles-repository=https://github.com/Oberacda/dotfiles.git --dotfiles-install-command=install.sh --remove-existing-container'
