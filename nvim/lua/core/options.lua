b7de5f2a1f65abf5810457e53123a4891fbafd63
David Oberacker <oberacker@fzi.de>
2025-06-24 10:08:32 +0200

    Update nvim config.

---
+++ nvim/lua/core/options.lua
@@ -0,0 +1,25 @@
+vim.opt.syntax = "on"
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.encoding = "utf-8"
+vim.opt.compatible = false
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.foldmethod = "syntax"
+vim.opt.foldlevel = 99
+vim.opt.relativenumber = true
+vim.opt.number = true
+
+vim.opt.colorcolumn = { 80, 100 }
+vim.opt.ruler = true
+
+vim.opt.spelllang = "en_us"
+vim.opt.backspace = { "indent", "eol", "start" }
+vim.opt.showcmd = true
+
+vim.opt.mouse = "a"
+vim.g.lazyvim_check_order = false
--- nvim/lua/plugins/nvim-notify.lua
+++ nvim/lua/plugins/nvim-notify.lua
@@ -1,5 +1,6 @@
 return {
   "rcarriga/nvim-notify",
+  event = "VeryLazy",
   keys = {
     {
       "<leader>un",
@@ -22,12 +23,4 @@ return {
       vim.api.nvim_win_set_config(win, { zindex = 100 })
     end,
   },
-  init = function()
-    -- when noice is not enabled, install notify on VeryLazy
-    if not LazyVim.has("noice.nvim") then
-      LazyVim.on_very_lazy(function()
-        vim.notify = require("notify")
-      end)
-    end
-  end,
 }
